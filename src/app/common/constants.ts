cbe707d435f9fe6744f7cf0bf1425e0c11e06c30
Mox Muriz <mox.muriz@hoelderlin.com>
2021-08-07 21:45:09 +0200

    Remove obsolete constants

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -3,14 +3,8 @@ import { environment } from 'src/environments/environment';
 export const SYNC_INTERVAL_MS = 5000;
 export const LONG_CLICK_INTERVAL_MS = 400;
 
-// TODO Paul Bauknecht 07 08 2021: Delete old crap
 export const API_URL = environment.backendUrl + '/api';
-export const TO_DO_LISTS_ENDPOINT_URL = environment.backendUrl + '/api/to-do-lists/';
-export const TO_DOS_ENDPOINT_URL = environment.backendUrl + '/api/to-dos/';
 export const PLACEHOLDER_ADD_NEW_TO_DO_LIST = 'Neue Liste anlegen';
 export const PLACEHOLDER_ADD_NEW_TO_DO = 'Neuen Eintrag hinzufügen';
 
-export const PRIORITIZATION_DEBOUNCE_TIME_IN_MILLIS = 3000;
-export const SPINNER_DEBOUNCE_TIME_IN_MILLIS = 50;
 export const ERROR_DISPLAY_TIME_IN_MILLIS = 5000;
-
