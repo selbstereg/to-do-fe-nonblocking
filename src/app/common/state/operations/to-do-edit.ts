bad54080e40adbe9505d6fc5d30be76f33937086
Mox Muriz <mox.muriz@hoelderlin.com>
2021-08-07 18:48:19 +0200

    Implement ToDoEdit

---
+++ src/app/common/state/operations/to-do-edit.ts
@@ -0,0 +1,27 @@
+import {Operation} from './operation';
+import uuidv4 from 'uuid/v4';
+import {ToDoLists} from '../glob-state';
+
+
+export default class ToDoEdit implements Operation {
+  public endpoint = '/to-dos/edit';
+  public operationId: string;
+
+  constructor(
+    public toDoId: string,
+    public toDoName: string
+  ) {
+    this.operationId = uuidv4();
+  }
+
+  apply(globState: ToDoLists): void {
+    const toDoToEdit = globState.toDoLists
+      .flatMap(toDoList => toDoList.toDos)
+      .find(toDo => toDo.id === this.toDoId);
+
+    if (toDoToEdit) {
+      toDoToEdit.name = this.toDoName;
+    }
+  }
+
+}
--- src/app/to-do-list-page/drag-drop-list/to-do-ui/edit-modal/to-do-editor.component.ts
+++ src/app/to-do-list-page/drag-drop-list/to-do-ui/edit-modal/to-do-editor.component.ts
@@ -1,34 +1,41 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import {Component, Inject} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import {Synchronizer} from '../../../../common/state/synchronizer.service';
 import {ToDo} from '../../../../common/state/glob-state';
+import ToDoEdit from '../../../../common/state/operations/to-do-edit';
 
 @Component({
-    selector: 'to-do-editor',
-    templateUrl: './to-do-editor.component.html',
-    styleUrls: ['./to-do-editor.component.css']
+  selector: 'to-do-editor',
+  templateUrl: './to-do-editor.component.html',
+  styleUrls: ['./to-do-editor.component.css']
 })
 export class ToDoEditorComponent {
 
-    toDo: ToDo;
-    editedName = '';
-    editedDetails = '';
+  toDo: ToDo;
+  editedName = '';
+  editedDetails = '';
 
-    constructor(
-        private synchronizer: Synchronizer,
-        private dialogRef: MatDialogRef<ToDoEditorComponent>,
-        @Inject(MAT_DIALOG_DATA) private data: any) {
-            this.toDo = this.data.toDoToEdit;
-            this.editedName = this.toDo.name;
-    }
+  constructor(
+    private synchronizer: Synchronizer,
+    private dialogRef: MatDialogRef<ToDoEditorComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: any) {
+    this.toDo = this.data.toDoToEdit;
+    this.editedName = this.toDo.name;
+  }
 
-    saveToDo() {
-        this.toDo.name = this.editedName;
-        // this.synchronizer.addOperation(new ToDoEdit())
-        this.dialogRef.close();
-    }
+  saveToDo() {
+    this.toDo.name = this.editedName;
+    // this.toDo.description = this.editedDescription
+    this.synchronizer.addOperation(
+      new ToDoEdit(
+        this.toDo.id,
+        this.toDo.name
+      )
+    );
+    this.dialogRef.close();
+  }
 
-    cancel() {
-      this.dialogRef.close();
-    }
+  cancel() {
+    this.dialogRef.close();
+  }
 }
