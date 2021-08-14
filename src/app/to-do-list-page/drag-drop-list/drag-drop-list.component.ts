67a1526ed8f7d3b3e49bf206dfef8146bdb0a737
Mox Muriz <mox.muriz@hoelderlin.com>
2021-08-14 22:29:59 +0200

    Clean up drag-drop-list.component.ts

--- src/app/to-do-list-page/drag-drop-list/drag-drop-list.component.ts
+++ src/app/to-do-list-page/drag-drop-list/drag-drop-list.component.ts
@@ -2,11 +2,6 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import {ToDo} from '../../common/state/glob-state';
 
-// export interface PriorityUpdate {
-//   toDoId: number;
-//   priority: number;
-// }
-
 @Component({
   selector: 'drag-drop-list',
   templateUrl: './drag-drop-list.component.html',
@@ -17,11 +12,9 @@ export class DragDropListComponent {
   @Input() toDos: ToDo[] = [];
 
   @Output() toDoDeleted = new EventEmitter<string>();
-  // string[] is array of to-do ids
   @Output() orderChanged = new EventEmitter<string[]>();
 
   drop(event: CdkDragDrop<string[]>): void {
-
     moveItemInArray(
       this.toDos,
       this.mapToReverseOrder(event.previousIndex),
@@ -30,34 +23,11 @@ export class DragDropListComponent {
 
     const order: string[] = this.toDos.map(toDo => toDo.id);
     this.orderChanged.emit(order);
-    //   this.isDragging = false;
-    //   moveItemInArray(
-    //     this.toDos,
-    //     this.mapToReverseOrder(event.previousIndex),
-    //     this.mapToReverseOrder(event.currentIndex)
-    //   );
-    //   this.prioritizationDebounceTimer.stop();
-    //   this.prioritizationDebounceTimer.start(this.submitPriorityOrder);
   }
 
   mapToReverseOrder(index: number): number {
     return this.toDos.length - 1 - index;
   }
-
-  // submitPriorityOrder(): void {
-  //   const updates: PriorityUpdate[] = [];
-  //   this.toDos.forEach(
-  //     (toDo, index) => {
-  //       if (toDo.priority !== index) {
-  //         toDo.priority = index;
-  //         updates.push({toDoId: toDo.id, priority: toDo.priority});
-  //       }
-  //     }
-  //   );
-  //   if (updates.length && !this.isDragging) {
-  //     this.prioritizationChanged.emit(updates);
-  //   }
-  // }
 
   deleteToDo(toDo: ToDo): void {
     this.toDoDeleted.emit(toDo.id);
