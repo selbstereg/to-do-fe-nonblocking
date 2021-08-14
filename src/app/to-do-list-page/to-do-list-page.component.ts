2d945109da59c23c325d7fe93c53924e209739ac
Mox Muriz <mox.muriz@hoelderlin.com>
2021-08-14 22:28:34 +0200

    Clen up to-do-list-page.component.ts

--- src/app/to-do-list-page/to-do-list-page.component.ts
+++ src/app/to-do-list-page/to-do-list-page.component.ts
@@ -1,85 +1,33 @@
-import {Component, OnInit, Input, OnChanges, SimpleChanges, OnDestroy} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {faHeart} from '@fortawesome/fontawesome-free-regular';
 import {PLACEHOLDER_ADD_NEW_TO_DO} from '../common/constants';
-import {MatDialog} from '@angular/material';
-import {filter} from 'rxjs/operators';
 import {ToDo, ToDoList} from '../common/state/glob-state';
 import {Synchronizer} from '../common/state/synchronizer.service';
 import ToDoAdd from '../common/state/operations/to-do-add';
-import {Subscription} from 'rxjs';
 import ToDoDelete from '../common/state/operations/to-do-delete';
+import {LoggingService} from '../common/logging/logging.service';
 
 @Component({
   selector: 'to-do-list-page',
   templateUrl: './to-do-list-page.component.html',
   styleUrls: ['./to-do-list-page.component.css']
 })
-export class ToDoListPageComponent  implements OnInit/*, OnChanges */, OnDestroy {
+export class ToDoListPageComponent {
 
   @Input() selectedToDoList: ToDoList;
 
   readonly ITEM_ADDER_PLACEHOLDER = PLACEHOLDER_ADD_NEW_TO_DO;
   readonly faHeart = faHeart;
-  private globStateSubscription: Subscription;
   toDos: ToDo[] = [];
 
 
   constructor(
-    private synchronizer: Synchronizer
-  //   private dialogService: MatDialog
-  ) {
-  //    this.addToDo = this.addToDo.bind(this);
-  //    this.fetchToDos = this.fetchToDos.bind(this);
-  }
-
-  ngOnInit(): void {
-    // this.globStateSubscription = this.synchronizer.subscribe(
-    //   lists => this.setToDos(lists)
-    // );
-  }
-
-  ngOnDestroy(): void {
-    this.globStateSubscription.unsubscribe(); // TODO Paul Bauknecht 06 06 2021: check if you forgot cleanup somewhere, e.g. in logging comp
-  }
-
-  // ngOnInit(): void {
-  //   this.fetchToDos();
-  // }
-
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   if (this.selectedToDoListChanged(changes)) {
-  //     this.fetchToDos();
-  //   }
-  // }
-
-  // toDoListIsEinkaufsliste(): boolean {
-  //   return this.selectedToDoList.name.toUpperCase().includes('EINKAUF');
-  // }
-
-  onRefresh() {
-  //   this.fetchToDos();
-  }
-
-  // openFavouriteEinkaufItemsDialog(): void {
-  //   this.dialogService.open(FavouriteEinkaufItems)
-  //     .afterClosed()
-  //     .pipe(
-  //       filter(val => !!val)
-  //     )
-  //     .subscribe(
-  //       (selectedItems: string[]) => selectedItems.forEach(this.addToDo)
-  //     );
-  // }
-
-  // private fetchToDos(): void {
-  //   this.crudClient.fetchToDos(this.selectedToDoList.id).subscribe(
-  //     (toDos: ToDo[]) => {
-  //       this.toDos = toDos;
-  //     }
-  //   );
-  // }
+    private synchronizer: Synchronizer,
+    private log: LoggingService
+  ) { }
 
   addToDo(toDoName: string): void {
+    this.log.info('addToDo: ' + this.toDos.toString());
     this.onOrderChanged(
       this.toDos.map(toDo => toDo.id)
     );
@@ -89,15 +37,7 @@ export class ToDoListPageComponent  implements OnInit/*, OnChanges */, OnDestroy
         this.selectedToDoList.id
       )
     );
-
-    // const toDo: ToDo = { name: toDoName, details: '', priority: this.calcHighestPrioPlusOne(), id: null };
-    // this.toDos.push(toDo);
-    // this.crudClient.addToDo(this.selectedToDoList.id, toDo).subscribe(
-    //   this.fetchToDos,
-    //   this.fetchToDos
-    // );
   }
-
 
   onToDoDeleted(toDoId: string): void {
     this.synchronizer.addOperation(
@@ -111,22 +51,5 @@ export class ToDoListPageComponent  implements OnInit/*, OnChanges */, OnDestroy
   onOrderChanged(toDoIdOrder: string[]): void {
     this.synchronizer.memorizeOrder(this.selectedToDoList.id, toDoIdOrder);
   }
-  // onPriorizationChanged(updates: PriorityUpdate[]): void {
-  //   this.crudClient.updatePriorities(updates).subscribe(
-  //     this.fetchToDos,
-  //     this.fetchToDos
-  //   );
-  // }
 
-  // private selectedToDoListChanged(changes: SimpleChanges) {
-  //   return changes.selectedToDoList;
-  // }
-
-  // private calcHighestPrioPlusOne(): number {
-  //   const priorities: number[] = this.toDos.map(toDo => toDo.priority);
-  //   if (!priorities.length) {
-  //     return 0;
-  //   }
-  //   return Math.max(...priorities) + 1;
-  // }
 }
