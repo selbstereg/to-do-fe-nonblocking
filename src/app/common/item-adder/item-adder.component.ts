9483d8b7305156bd0b8f79a3108792191771db02
Mox Muriz <mox.muriz@hoelderlin.com>
2021-10-22 22:07:44 +0200

    Add debug output for input field issue

--- src/app/common/item-adder/item-adder.component.ts
+++ src/app/common/item-adder/item-adder.component.ts
@@ -1,15 +1,19 @@
-import {Component, Output, EventEmitter, Input, ViewChild, ElementRef} from '@angular/core';
+import {Component, Output, EventEmitter, Input, ViewChild, ElementRef, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import {LoggingService} from '../logging/logging.service';
 
 @Component({
   selector: 'item-adder',
   templateUrl: './item-adder.component.html',
   styleUrls: ['./item-adder.component.css']
 })
-export class ItemAdderComponent {
+export class ItemAdderComponent implements OnInit, OnChanges {
   // not using ngModel here because of bug where old text spontaneously appears again some time after item was added
   @ViewChild('input', {static: false}) input: ElementRef;
   @Input() placeholder = '';
   @Output() addItem = new EventEmitter<string>();
+
+  constructor(private log: LoggingService) {
+  }
 
   onAddItem() {
     const inputText: string = this.input.nativeElement.value;
@@ -17,6 +21,15 @@ export class ItemAdderComponent {
       this.addItem.emit(this.input.nativeElement.value);
     }
     this.input.nativeElement.value = '';
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.log.info(`ItemAdder onChanges - changes=${JSON.stringify(changes)}`);
+    this.log.info(`ItemAdder onChanges - input=${this.input.nativeElement.value}`);
+  }
+
+  ngOnInit(): void {
+    this.log.info(`ItemAdder onInit - input=${this.input.nativeElement.value}`);
   }
 
 }
